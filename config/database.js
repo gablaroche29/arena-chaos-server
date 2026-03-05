0906ee068583488fe69b53322d71de1b5b7bfdd7
Gabriel Laroche <144166325+gablaroche29@users.noreply.github.com>
2026-03-05 09:57:28 -0500

    feat: update bd and remove bd from git

--- config/database.js
+++ config/database.js
@@ -3,13 +3,28 @@ import Database from "better-sqlite3";
 const db = new Database("events.db");
 
 db.exec(`
-  CREATE TABLE IF NOT EXISTS events (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    type TEXT NOT NULL,
-    payload TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-  )
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        score INTEGER DEFAULT 0,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        type TEXT NOT NULL,
+        name TEXT NOT NULL,
+        processed INTEGER DEFAULT 0,
+        vote_count INTEGER DEFAULT 0,
+        vote_required INTEGER DEFAULT 3,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    CREATE TABLE IF NOT EXISTS votes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        event_id INTEGER,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        UNIQUE(user_id, event_id)
+    );
 `);
 
 export default db;
